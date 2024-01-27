bcd6f0a4ce4b5eb27258ddad342d2eb7a1b75c60
Ani-Torosyan <153121357+Ani-Torosyan@users.noreply.github.com>
2024-01-27 22:58:04 +0400

    Add project files.

---
+++ Exc.1/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace HomeWork_2
+{
+    internal class Program
+    {
+        static void findMidElement(List<int> list)
+        {
+            int len = list.Count();
+            if (len % 2 == 0) 
+            {
+                Console.WriteLine("The middle elements are: " + list[(len / 2) - 1] + " and " + list[(len / 2)]);
+            }
+            else Console.WriteLine("The middle element is: " +list[(len / 2)]);
+        }
+        static void Main(string[] args)
+        {
+            List<int> oddList = new List<int>() { 1, 2, 3};
+            List<int> evenList = new List<int>() { 1, 2, 3, 4};
+
+            findMidElement(oddList);  
+            findMidElement(evenList);
+        }
+    }
+}
---
+++ Exc.2/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace HomeWork_2
+{
+    internal class Program
+    {
+        static int findMax(List<int> list)
+        {
+            int max = list[0];
+            foreach (var item in list)
+            {
+                if (item > max)
+                {
+                    max = item;
+                }
+            }
+            return max;
+        }
+        static void Main(string[] args)
+        {
+            List<int> list = new List<int>() { 1, 2, 3 };
+            int max = findMax(list);
+
+            Console.WriteLine("The largest element in the list is: " + max);
+        }
+    }
+}
